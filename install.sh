0d65b09a30ae71101916f4e8ce19e3cd063653dd
maximilionus <maximilionuss@gmail.com>
2023-12-30 18:40:06 +0300

    Add installer shell script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+echo "-> Installing the i3 configurations set."
+
+sources=("i3" "picom" "polybar" "rofi")
+targets=("$HOME/.config" "$HOME/.config" "$HOME/.config" "$HOME/.config")
+
+for ((i=0; i<${#targets[@]}; i++)); do
+    target="${targets[i]}"
+    source="${sources[i]}"
+
+    echo "--> Installing configuration for \"$source\"."
+    ln -sf "$(realpath "$source")" "$target"
+done
+
+echo "-> i3 configuration set successfully installed,"
+echo "   restart your shell to load the changes."
