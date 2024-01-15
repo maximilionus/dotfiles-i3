c1d4a9200140930183adbf8f6a8eec5c71533596
maximilionus <maximilionuss@gmail.com>
2024-01-15 20:33:28 +0300

    polybar/playerctl: Implement dynamic output format
    Metadata does not always contain even the 2 necessary values like author and
    title, so the script has been improved to generate the most dynamic output
    possible, ready for the different variations of "playerctl" output.

--- polybar/scripts/playerctl.sh
+++ polybar/scripts/playerctl.sh
@@ -1,19 +1,42 @@
 #!/bin/bash
 
+print_metadata() {
+    local icon="$1"
+    local color="$2"
+    local artist="$3"
+    local title="$4"
 
-# Stop the exec if 'playerctl' is not detected
+    local metadata
+
+    if [[ -n "$artist" && -n "$title" ]]; then
+        metadata="%{F$color}$icon $artist - $title%{F-}"
+    elif [[ -n "$artist" ]]; then
+        metadata="%{F$color}$icon $artist%{F-}"
+    elif [[ -n "$title" ]]; then
+        metadata="%{F$color}$icon $title%{F-}"
+    else
+        metadata="%{F$color}$icon Unknown%{F-}"
+    fi
+
+    echo "$metadata"
+}
+
+# Check if playerctl is installed
 if ! command -v playerctl &> /dev/null; then
     exit 1
 fi
 
 playerctl_status=$(playerctl status 2> /dev/null)
 
-if [[ -z "$playerctl_status" || "$playerctl_status" == "Stopped" ]]; then
+if [[ -z "$playerctl_status" || $playerctl_status == "Stopped" ]]; then
     echo ""
-elif [[ "$playerctl_status" == "Playing" ]]; then
-    metadata=$(playerctl metadata --format "󰐊 {{ artist }} - {{ title }}")
-    echo "%{F#7366F8}$metadata%{F-}"
 else
-    metadata=$(playerctl metadata --format "󰏤 {{ artist }} - {{ title }}")
-    echo "%{F#808080}$metadata%{F-}"
+    artist=$(playerctl metadata --format "{{ artist }}")
+    title=$(playerctl metadata --format "{{ title }}")
+
+    if [[ $playerctl_status == "Playing" ]]; then
+        print_metadata "󰐊" "#7366F8" "$artist" "$title"
+    else
+        print_metadata "󰏤" "#808080" "$artist" "$title"
+    fi
 fi
