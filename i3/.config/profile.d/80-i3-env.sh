0a3330ba4730e998908041203f85c40ef7390962
maximilionus <maximilionuss@gmail.com>
2026-01-15 18:00:54 +0300

    i3/profile.d: Export XDG_CURRENT_DESKTOP

--- i3/.config/profile.d/80-i3-env.sh
+++ i3/.config/profile.d/80-i3-env.sh
@@ -1,3 +1,4 @@
+export XDG_CURRENT_DESKTOP=i3
 export I3_CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/i3"
 export I3_SCRIPTS_DIR="$I3_CONFIG_DIR/scripts"
 export I3_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/var/run/user/$UID}/i3"
