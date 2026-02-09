56d215d64bb42149dc6c8d6af7d8a58fcca2eb33
maximilionus <maximilionuss@gmail.com>
2026-02-09 23:01:37 +0300

    i3: Improve exec-cwd syntax

--- i3/.config/i3/scripts/exec-cwd.sh
+++ i3/.config/i3/scripts/exec-cwd.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #
-# Execute any provided command in the current working directory of the focused
-# i3 window.
+# Execute the specified command in the current working directory of the active
+# X11 window.
 
-cwd=""
-window_id=$(i3-msg -t get_tree | jq '.. | objects | select(.focused==true) | .window')
-window_pid=$(xprop -id $window_id | grep _NET_WM_PID | grep -oP "\d+")
-child_pid=$(pgrep -P $window_pid | tail -n 1)
+window_xid=$(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2)
+window_pid=$(xprop -id "$window_xid" _NET_WM_PID | grep -oP "\d+")
+child_pid=$(pgrep -P "$window_pid" | tail -n 1)
 
-if [[ ! -z "$child_pid" ]]; then
+if [[ -n "$child_pid" ]]; then
     cwd=$(readlink -f "/proc/$child_pid/cwd")
     cd "$cwd"
 fi
